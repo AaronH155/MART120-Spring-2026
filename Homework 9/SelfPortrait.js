b2b01f46a29611904012203a0d1754474e7ffc3a
AaronH155 <aaronheisel155@gmail.com>
2026-03-31 22:58:03 -0600

    Homework 9

---
+++ Homework 9/SelfPortrait.js
@@ -0,0 +1,97 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function draw() {
+  background(220);
+  noStroke()
+  textSize(12);
+  text("x: " + mouseX + ", y: " + mouseY, 10, 20);
+  
+  fill(0, 0, 0)
+  text("Aaron Heisel in art", 150, 50);
+  
+  fill(0, 0, 0)
+  text("Aaron Heisel", 10, 340);
+  
+  fill(51, 58, 107);
+  square(0, 300, 500)
+  fill(199, 160, 113);
+  triangle(141, 301, 195, 360, 240, 311);
+  
+  fill(255, 255, 255)
+  text("Aaron Heisel", 10, 340);
+  
+  strokeWeight(0);
+  stroke(212, 321, 123);
+  fill(237, 218, 194);
+  circle(200, 200, 250);
+  
+  strokeWeight(5);
+  stroke(0, 0, 0);
+  noFill();
+  circle(250, 170, 70);
+  
+  strokeWeight(5);
+  stroke(0, 0, 0);
+  noFill();
+  circle(150, 170, 70);
+  
+  line(187, 172, 215, 172);
+  line(114, 174, 84, 160);
+  line(286, 174, 321, 160);
+  
+  
+  
+  strokeWeight(0);
+  stroke(212, 321, 123);
+  fill(255, 255, 255);
+  circle(150, 170, 50);
+  
+  strokeWeight(0);
+  stroke(212, 321, 123);
+  fill(255, 255, 255);
+  circle(250, 170, 50);
+  
+  strokeWeight(0);
+  stroke(212, 321, 123);
+  fill(0, 0, 0);
+  circle(250, 170, 20);
+  
+  strokeWeight(0);
+  stroke(212, 321, 123);
+  fill(0, 0, 0);
+  circle(150, 170, 20);
+  
+
+  
+  ellipse(200,250,180,30,);
+  
+  fill(237, 218, 194);
+  ellipse(200,240,180,30,);
+  
+  fill(240, 208, 170);
+  ellipse(200,230,50,20,);
+  ellipse(200,210,20,50,);
+  
+  fill(92, 68, 39);
+  rotate(25)
+  ellipse(120,115,150,30,);
+  
+  
+  rotate(25)
+  ellipse(120,145,200,30,);
+  
+   
+  rotate(150)
+  ellipse(32,280,30,200,);
+  
+  
+
+}
+
+
